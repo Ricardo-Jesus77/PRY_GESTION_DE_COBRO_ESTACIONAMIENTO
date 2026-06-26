17e0db4503ac3e95e48dc9d047ef92bfa3f8c25a
Daniel Medina <danielmediv@gmail.com>
2026-06-26 09:44:26 -0600

    Se agrega subclase Automivil

--- Automovil.cs
+++ Automovil.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace PRY_GESTION_DE_COBRO_ESTACIONAMIENTO
 {
-    internal class Automovil
+    internal class Automovil:Vehiculo
     {
+        public abstract class automovil
+        {
+            public int Horas_estancia {  get; set; }
+            
+            public automovil(int horas)
+            {
+                Horas_estancia = horas;
+            }
+            public abstract double CalcularTarifa();
+        }
+
     }
 }
