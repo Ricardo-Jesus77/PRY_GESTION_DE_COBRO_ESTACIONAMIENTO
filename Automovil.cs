d0360264dfe3acd579a1f8eb1b6028cdaa32d590
Daniel Medina <danielmediv@gmail.com>
2026-06-26 09:10:35 -0600

    Se agrega la subclase Vehiculo

---
+++ Automovil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRY_GESTION_DE_COBRO_ESTACIONAMIENTO
+{
+    internal class Automovil
+    {
+    }
+}
